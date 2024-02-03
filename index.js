2a10fa524fd015036f0bc900b2da4acf768b1b01
kkebuka <kkebuka@gmail.com>
2024-02-03 14:23:50 +0100

    changes

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const openModal = document.querySelector('.open');
-const modalDiv = document.querySelector('modal-open');
+const modalDiv = document.querySelector('.modal-open');
 const closeModal = document.querySelector('.close');
 
 openModal.addEventListener('click',function(){
-    
+    modalDiv.style.visibility = 'visible';
+    alert('e work')
+})
+
+closeModal.addEventListener('click',function(){
+    modalDiv.style.visibility = 'hidden';
 })
